5c159ab46259f4320f3d7c989ec3270624ab6856
isabel <isabel.calzadilla.18@gmail.com>
2022-04-21 13:32:05 +0200

    step 2

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -18,7 +18,47 @@ namespace Variables
             Console.WriteLine($"Edad aproximada : { edad } ");
 
             */
-            
+
+
+            // SECOND STEP
+
+            int edadPersona1;
+            int edadPersona2;
+            int edadPersona3;
+            int edadPersona4;
+
+            // UNIFICACIÒN DE DECLARACION DE VARIABLES A UNA LÌNEA (DECLARACIÒN IMPLICITA DE VARIABLES)
+            edadPersona1 = edadPersona2 = edadPersona3 = edadPersona4 = 23;
+
+            Console.WriteLine(edadPersona4);
+            Console.WriteLine(edadPersona3);
+            Console.WriteLine(edadPersona2);
+            Console.WriteLine(edadPersona1);
+
+            //  CASTING
+
+           // Declaraciòn explicita de variables:
+
+            //conversiòn d eun tipo de variable a otro.
+
+            double temperature = 33.5;
+            int temperatureNormal;
+
+            temperatureNormal = (int)temperature; //CASTING DE DOUBLE A INTEGER
+
+            Console.WriteLine(temperatureNormal);
+            Console.WriteLine(temperature);
+
+
+            // Conversiòn de texto a numeral
+
+            Console.WriteLine("Induzca el primer valor");
+            int val1 = int.Parse(Console.ReadLine()); // CONVERSION DE DATOS 
+            Console.WriteLine(val1);
+            string val2 = "hola";
+            string aux = String.Concat(Console.ReadLine(), val2);
+            Console.WriteLine(aux);
+
         }
     }
 }
