1f7c3903d4ba6aa2fce9d055e1617fa9ea2e9589
isabel <isabel.calzadilla.18@gmail.com>
2022-04-21 10:35:19 +0200

    second repo

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 28;
+            Console.WriteLine(edad);
+        }
+    }
+}
