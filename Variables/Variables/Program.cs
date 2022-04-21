ba38705436cbeab2626dc56ed9b52c89daf60ac8
isabel <isabel.calzadilla.18@gmail.com>
2022-04-21 12:46:32 +0200

    Primer esbozo

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,8 +6,19 @@ namespace Variables
     {
         static void Main(string[] args)
         {
+            // FIRSTS STEPS
+
+            /*
             int edad = 28;
             Console.WriteLine(edad);
+            Console.WriteLine(8 * 96);
+            Console.WriteLine(5.0 / 2.5);
+            // Console.WriteLine("Edad aproximada : " + edad);
+            //  INTERPOLACION DE STRINGS
+            Console.WriteLine($"Edad aproximada : { edad } ");
+
+            */
+            
         }
     }
 }
