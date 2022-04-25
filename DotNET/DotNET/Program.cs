2910662b41a43aea0701ae471d385d6546894116
isabel <isabel.calzadilla.18@gmail.com>
2022-04-25 11:56:09 +0200

    REST interfacees

---
+++ DotNET/DotNET/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOTACIONES DEL FRAMEWORK ESPECÌFICO CON CONDICIONALES
+#if DNX451
+            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().FullName);
+#endif
+            Console.WriteLine("hello");
+
+#if DNXCORE50
+           Console.WriteLine("versiòn core");
+#endif
+            Console.WriteLine("hello");
+        }
+    }
+}
---
+++ DotNET/MVCdot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCdot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
