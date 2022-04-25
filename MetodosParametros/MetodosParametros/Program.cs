20e051503286aa8de02144e6a15aaf86e2d6e803
isabel <isabel.calzadilla.18@gmail.com>
2022-04-25 16:42:09 +0200

    mètodos opcionales

--- MetodosParametros/MetodosParametros/Program.cs
+++ MetodosParametros/MetodosParametros/Program.cs
@@ -11,6 +11,7 @@ namespace MetodosParametros
             Console.WriteLine(firtsMethod(5, 6));
             Console.WriteLine(sumaValor(5, 6));
             Console.WriteLine(sumaValor(5, 6, 32));
+            Console.WriteLine(couldBe(5, 6, 9));
         }
 
         /*        static void firtsMethod(int a, int b)
@@ -27,5 +28,12 @@ namespace MetodosParametros
         static int sumaValor(int a1, int a2) => a1 * a2; //SÌNTESIS DE MÈTODOS
 
         static int sumaValor(int a1, int a2, int a3) => a1 * a2 + a3; //SÌNTESIS DE MÈTODOS
+
+
+        // PARÀMETROS OPCIONALES
+
+        static double couldBe(double a, double b, double c = 0) => a * b + c; // LA SINTÀXIS PARA QUE UN PARÀMETRO SEA OPCIONAL SE DEBE DE INDICAR EN EL LA PROPIA RECEPCION DEL MÈTODO E INICIALIZADO EL PARÀMETRO EN '0' O VACÌO, SEGÙN APLIQUE.
+
+        // LOS PARÀMETROS OPCIONALES SIEMPRE DEBEN DE SEGUIR A LOS OBLIGATORIOS
     }
 }
