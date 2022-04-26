e3f8428b5a5f7ce330f0b6fd63d9e66e37da207a
isabel <isabel.calzadilla.18@gmail.com>
2022-04-26 08:51:29 +0200

    IF + ternario

---
+++ CondicionalesIF/CondicionalesIF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CondicionalesIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  bool isCold = false;
+
+            //Console.WriteLine(!isCold);
+            int age = 21;
+            Console.WriteLine("Age evaluator ");
+            Console.WriteLine("Give me your age, please");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine(ageEvaluator(age));
+
+        }
+
+        static String ageEvaluator(int age) => age < 18 ? "Eres menor de edad " : "Eres mayor de edad ";
+    }
+}
