5c00b9fdda0ef806da37bbdf6b5295bc9efb5b2a
isabel <isabel.calzadilla.18@gmail.com>
2022-05-02 10:26:27 +0200

    Clase circulo

---
+++ POO_On_CSharp/POO_On_CSharp/ClsCirculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_On_CSharp
+{
+    class Circulo
+    {
+        public double PI = Math.PI;
+
+        public double calculaArea(int radio)
+        {
+            return PI * radio * radio;
+        }
+    }
+}
---
+++ POO_On_CSharp/POO_On_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using POO_On_CSharp; // importar los namespace
+
+namespace POO_On_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Circulo circle = new Circulo();
+            Console.WriteLine(circle.PI);
+            Console.WriteLine(circle.calculaArea(5));
+        }
+    }
+
+    // ADICIÒN DE LAS CLASES
+
+    
+}
